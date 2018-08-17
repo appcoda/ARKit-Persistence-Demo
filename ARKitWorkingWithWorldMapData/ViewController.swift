e2e05c6e6ae00513c8fa5b9a4ce1ecfc0a730819
Simon Ng <simonng@appcoda.com>
2018-08-17 17:25:02 +0800

    Initial version

---
+++ ARKitWorkingWithWorldMapData/ViewController.swift
@@ -0,0 +1,162 @@
+//
+//  ViewController.swift
+//  ARKitWorkingWithWorldMapData
+//
+//  Created by Macbook on 8/11/18.
+//  Copyright © 2018 Jayven Nhan. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var label: UILabel!
+    
+    var worldMapURL: URL = {
+        do {
+            return try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                .appendingPathComponent("worldMapURL")
+        } catch {
+            fatalError("Error getting world map URL from document directory.")
+        }
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        configureLighting()
+        addTapGestureToSceneView()
+    }
+    
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didReceiveTapGesture(_:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func didReceiveTapGesture(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: sceneView)
+        guard let hitTestResult = sceneView.hitTest(location, types: [.featurePoint, .estimatedHorizontalPlane]).first
+            else { return }
+        let anchor = ARAnchor(transform: hitTestResult.worldTransform)
+        sceneView.session.add(anchor: anchor)
+    }
+    
+    func generateSphereNode() -> SCNNode {
+        let sphere = SCNSphere(radius: 0.05)
+        let sphereNode = SCNNode()
+        sphereNode.position.y += Float(sphere.radius)
+        sphereNode.geometry = sphere
+        return sphereNode
+    }
+    
+    func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        resetTrackingConfiguration()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    @IBAction func resetBarButtonItemDidTouch(_ sender: UIBarButtonItem) {
+        resetTrackingConfiguration()
+    }
+    
+    @IBAction func saveBarButtonItemDidTouch(_ sender: UIBarButtonItem) {
+        sceneView.session.getCurrentWorldMap { (worldMap, error) in
+            guard let worldMap = worldMap else {
+                return self.setLabel(text: "Error getting current world map.")
+            }
+            
+            do {
+                try self.archive(worldMap: worldMap)
+                DispatchQueue.main.async {
+                    self.setLabel(text: "World map is saved.")
+                }
+            } catch {
+                fatalError("Error saving world map: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func loadBarButtonItemDidTouch(_ sender: UIBarButtonItem) {
+        guard let worldMapData = retrieveWorldMapData(from: worldMapURL),
+            let worldMap = unarchive(worldMapData: worldMapData) else { return }
+        resetTrackingConfiguration(with: worldMap)
+    }
+    
+    func resetTrackingConfiguration(with worldMap: ARWorldMap? = nil) {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal]
+        
+        let options: ARSession.RunOptions = [.resetTracking, .removeExistingAnchors]
+        if let worldMap = worldMap {
+            configuration.initialWorldMap = worldMap
+            setLabel(text: "Found saved world map.")
+        } else {
+            setLabel(text: "Move camera around to map your surrounding space.")
+        }
+        
+        sceneView.debugOptions = [.showFeaturePoints]
+        sceneView.session.run(configuration, options: options)
+    }
+    
+    func setLabel(text: String) {
+        label.text = text
+    }
+    
+    func archive(worldMap: ARWorldMap) throws {
+        let data = try NSKeyedArchiver.archivedData(withRootObject: worldMap, requiringSecureCoding: true)
+        try data.write(to: self.worldMapURL, options: [.atomic])
+    }
+    
+    func retrieveWorldMapData(from url: URL) -> Data? {
+        do {
+            return try Data(contentsOf: self.worldMapURL)
+        } catch {
+            self.setLabel(text: "Error retrieving world map data.")
+            return nil
+        }
+    }
+    
+    func unarchive(worldMapData data: Data) -> ARWorldMap? {
+        guard let unarchievedObject = try? NSKeyedUnarchiver.unarchivedObject(ofClass: ARWorldMap.self, from: data),
+            let worldMap = unarchievedObject else { return nil }
+        return worldMap
+    }
+    
+}
+
+
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard !(anchor is ARPlaneAnchor) else { return }
+        let sphereNode = generateSphereNode()
+        DispatchQueue.main.async {
+            node.addChildNode(sphereNode)
+        }
+    }
+    
+}
+
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
+}
+
+extension UIColor {
+    open class var transparentWhite: UIColor {
+        return UIColor.white.withAlphaComponent(0.70)
+    }
+}
